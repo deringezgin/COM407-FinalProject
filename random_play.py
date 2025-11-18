8e17d078f3141f8417593f04159c8c885c17c631
Derin Gezgin <dgezgin@conncoll.edu>
2025-11-18 13:13:26 -0500

    Adding better game state formatting for readability improvment.

--- random_play.py
+++ random_play.py
@@ -1,5 +1,6 @@
 import sys
 import random
+from game_utils import format_state
 
 # Ensure that the library path is available
 PW_PYTHON_PATH = "planet-wars-rts/app/src/main/python"
@@ -12,8 +13,7 @@ from core.game_state import Action  # type: ignore
 class RandomPlay(PlanetWarsPlayer):
     def get_action(self, game_state):
         # Get all the idle planets that are owned by the random player
-        print("\n" * 100)
-        print(game_state)
+        print(format_state(game_state))
         idle_planets = [p for p in game_state.planets if p.owner == self.player and p.transporter is None]
         if not idle_planets:  # If no idle planets, do nothing
             return Action.do_nothing()
